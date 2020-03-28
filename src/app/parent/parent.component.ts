5e38b7672cb59db44ebc5ab9da23ba0cb0163bad
nizamwordpress <nizamwordpress@gmail.com>
2020-03-28 23:34:23 +0530

    2 files modified, 6 added

---
+++ src/app/parent/parent.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+  @ViewChild(ChildComponent) child;
+  parentmessage = "Parent To child"
+  message:string;
+  constructor() { }
+
+  ngAfterViewInit() {
+    
+  }
+  ngOnInit() {
+    this.message = this.child.message
+  }
+
+}
